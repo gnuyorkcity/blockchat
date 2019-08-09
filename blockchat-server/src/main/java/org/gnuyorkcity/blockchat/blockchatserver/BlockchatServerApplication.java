3b1e2ca297c5b6f8d67ea49b1a8059c66f52dcd6
Matthew Arnold <matthew.brandon.arnold@gmail.com>
2019-08-09 18:07:11 -0400

    Spring Boot server added

---
+++ blockchat-server/src/main/java/org/gnuyorkcity/blockchat/blockchatserver/BlockchatServerApplication.java
@@ -0,0 +1,13 @@
+package org.gnuyorkcity.blockchat.blockchatserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockchatServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlockchatServerApplication.class, args);
+    }
+
+}
---
+++ blockchat-server/src/test/java/org/gnuyorkcity/blockchat/blockchatserver/BlockchatServerApplicationTests.java
@@ -0,0 +1,16 @@
+package org.gnuyorkcity.blockchat.blockchatserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BlockchatServerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
